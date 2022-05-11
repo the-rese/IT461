d7117739696e5c6c995d0be7f68c8d72a351a58e
Therese Bolabola <terebol0314@gmail.com>
2022-05-11 09:48:49 +0800

    Implements fields and metadata on /cats and /cats/<id>

--- server/dockers/db-schema/pet.sql
+++ server/dockers/db-schema/pet.sql
@@ -3,15 +3,15 @@ CREATE DATABASE pets;
 USE pets;
 
 CREATE TABLE dogs(
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(200) NOT NULL,
-    PRIMARY KEY (id),
+    id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(200) NOT NULL, 
+    PRIMARY KEY (id), 
     INDEX (name)
-);
+    );
 
-CREATE TABLE cats(
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(200) NOT NULL,
-    PRIMARY KEY (id),
+CREATE TABLE cats( 
+    id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(200) NOT NULL, 
+    PRIMARY KEY (id), 
     INDEX (name)
-);
+    );
