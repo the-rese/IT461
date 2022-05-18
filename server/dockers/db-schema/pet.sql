8d1fcd5f97acbbd9742718514239764e7a432614
Therese Bolabola <terebol0314@gmail.com>
2022-05-18 21:30:23 +0800

    Update pet.sql

--- server/dockers/db-schema/pet.sql
+++ server/dockers/db-schema/pet.sql
@@ -16,4 +16,11 @@ CREATE TABLE cats(
     INDEX (name)
     );
 
+CREATE TABLE users(
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(200) NOT NULL,
+    password VARCHAR(200) NOT NULL,
+    PRIMARY KEY (id),
+    INDEX (username)
+);
 INSERT INTO users (username, password) VALUES ('admin', 'admin');
